dadc964a37f4acf9796c23402b483338589a7c13
greyspectrum <hello@greyspectrum.io>
2019-02-26 16:35:59 -0500

    update README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gocat
-a quick netcat implementation in go
+A crude netcat implementation in go.
+Inspiration from [Hacking With Go](https://github.com/parsiya/Hacking-with-Go) and [Black Hat Python](https://github.com/WillPennell/Python/tree/master/Black-Hat-Python).
--- main.go
+++ main.go
@@ -3,16 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"flag"
+	"io"
+	"net"
 )
 
 var (
 	host, port string
 )
 
+func handleConnectionNoLog(conn net.Conn) {
+
+    rAddr := conn.RemoteAddr().String()
+    defer fmt.Printf("Closed connection from %v\n", rAddr)
+
+    // This will accomplish the echo if we do not want to log
+    io.Copy(conn, conn)
+}
+
 func main() {
 
 	if len(os.Args) != 3 {
-		fmt.Println("Usage:", os.Args[0], "host", "port")
+		fmt.Println("Usage:", os.Args[0], "<host>", "<port>")
 		return
 	}
 
@@ -21,4 +33,10 @@ func main() {
 
 	fmt.Println(host)
 	fmt.Println(port)
+
+	flag.Parse()
+
+	// Convert host and port to host:port
+	t := net.JoinHostPort(host, port)
+
 }
